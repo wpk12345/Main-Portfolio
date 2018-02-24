7d9efd98a5b2bf11950bc68ae178e0b99e6038ce
William King <wpk12345@gmail.com>
2018-02-24 18:12:11 -0500

    put js in separte file and linked jquery at bottom

---
+++ assets/js/app.js
@@ -0,0 +1,61 @@
+$(document).ready(function () {
+    //Logic for h1 delay
+
+    $("#one").hide(0).delay(1500).show(0);
+    $("#two").hide(0).delay(1800).show(0);
+    $("#three").hide(0).delay(2100).show(0);
+    $("#four").hide(0).delay(2400).show(0);
+    $("#five").hide(0).delay(2500).show(0);
+    $("#six").hide(0).delay(2600).show(0);
+    $("#seven").hide(0).delay(2700).show(0);
+    $("#eight").hide(0).delay(2800).show(0);
+    $("#nine").hide(0).delay(2900).show(0);
+    $("#ten").hide(0).delay(3000).show(0);
+    $("#eleven").hide(0).delay(3100).show(0);
+    $("#twelve").hide(0).delay(3200).show(0);
+    $("#thirteen").hide(0).delay(3300).show(0);
+    $("#fourteen").hide(0).delay(3400).show(0);
+    $("#fifteen").hide(0).delay(3500).show(0);
+    $("#sixteen").hide(0).delay(3600).show(0);
+    $("#seventeen").hide(0).delay(3700).show(0);
+    $("#eighteen").hide(0).delay(4000).show(0);
+    $("#nineteen").hide(0).delay(4300).show(0);
+    $("#twenty").hide(0).delay(4600).show(0);
+    $("#twentyone").hide(0).delay(4900).show(0);
+
+    //Logic for h2 delay
+
+    $("#hone").hide(0).delay(5400).show(0);
+    $("#htwo").hide(0).delay(5700).show(0);
+    $("#hthree").hide(0).delay(6000).show(0);
+    $("#hfour").hide(0).delay(6300).show(0);
+    $("#hfive").hide(0).delay(6600).show(0);
+    $("#hsix").hide(0).delay(6700).show(0);
+    $("#hseven").hide(0).delay(6800).show(0);
+    $("#height").hide(0).delay(6900).show(0);
+    $("#hnine").hide(0).delay(7000).show(0);
+    $("#hten").hide(0).delay(7100).show(0);
+    $("#heleven").hide(0).delay(7200).show(0);
+    $("#htwelve").hide(0).delay(7300).show(0);
+    $("#hthirteen").hide(0).delay(7400).show(0);
+    $("#hfourteen").hide(0).delay(7500).show(0);
+    $("#hfifteen").hide(0).delay(7600).show(0);
+    $("#hsixteen").hide(0).delay(7700).show(0);
+    $("#hseventeen").hide(0).delay(7800).show(0);
+    $("#heighteen").hide(0).delay(7900).show(0);
+    $("#hnineteen").hide(0).delay(8000).show(0);
+    $("#htwenty").hide(0).delay(8100).show(0);
+    $("#htwentyone").hide(0).delay(8200).show(0);
+    $("#htwentytwo").hide(0).delay(8300).show(0);
+    $("#htwentythree").hide(0).delay(8400).show(0);
+    $("#htwentyfour").hide(0).delay(8500).show(0);
+    $("#htwentyfive").hide(0).delay(8600).show(0);
+    $("#htwentysix").hide(0).delay(8700).show(0);
+    $("#htwentyseven").hide(0).delay(8800).show(0);
+    $("#htwentyeight").hide(0).delay(8900).show(0);
+    $("#htwentynine").hide(0).delay(9000).show(0);
+    $("#hthirty").hide(0).delay(9300).show(0);
+    $("#hthirtyone").hide(0).delay(9600).show(0);
+    $("#hthirtytwo").hide(0).delay(9900).show(0);
+    $("#hthirtythree").hide(0).delay(10200).show(0);
+});
